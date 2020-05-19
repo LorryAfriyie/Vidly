f36235356b5c83e8dd941f7a39016e8f382ec847
LorryAfriyie <36528903+LorryAfriyie@users.noreply.github.com>
2020-05-19 14:02:11 +0200

    Birthday changes to database

---
+++ Vidly/Migrations/202005191145167_AddBirthdateToCustomer.cs
@@ -0,0 +1,20 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdateToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "Birthday", c => c.DateTime());
+            Sql("UPDATE Customers SET Birthday = 9/11/1994 WHERE Id = 1");
+
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "Birthday");
+        }
+    }
+}
